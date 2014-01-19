6a3e6bef0e87ab0400ed34a76d14405e2c3d3e73
dblevins <david.blevins@gmail.com>
2014-01-18 16:17:30 -0800

    Start of some examples

---
+++ toolz/src/main/java/org/example/toolz/App.java
@@ -0,0 +1,55 @@
+package org.example;
+
+import org.tomitribe.crest.api.Command;
+import org.tomitribe.crest.api.Default;
+import org.tomitribe.crest.api.Option;
+import org.tomitribe.crest.api.StreamingOutput;
+import org.tomitribe.crest.util.Files;
+import org.tomitribe.crest.util.IO;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
+
+/**
+ *
+ */
+public class App {
+
+    @Command
+    public StreamingOutput cat(final File file) {
+        Files.exists(file);
+        Files.readable(file);
+        Files.file(file);
+
+        return new StreamingOutput() {
+            @Override
+            public void write(OutputStream os) throws IOException {
+                IO.copy(file, os);
+            }
+        };
+    }
+
+    @Command
+    public String hello(@Option("name") @Default("World") String name,
+                        @Option("language") @Default("EN") Language language) {
+        return String.format("%s, %s!", language.getSalutation(), name);
+    }
+
+    public static enum Language {
+        EN("Hello"),
+        ES("Hola"),
+        FR("Bonjour");
+
+        private final String salutation;
+
+        private Language(String salutation) {
+            this.salutation = salutation;
+        }
+
+        public String getSalutation() {
+            return salutation;
+        }
+    }
+
+}
---
+++ toolz/src/test/java/org/example/toolz/AppTest.java
@@ -0,0 +1,22 @@
+package org.example;
+
+
+import org.apache.xbean.finder.archive.ClassesArchive;
+import org.junit.Assert;
+import org.junit.Test;
+import org.tomitribe.crest.Main;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest extends Assert {
+
+    @Test
+    public void testApp() throws Exception {
+        final Main main = new Main(new ClassesArchive(App.class));
+
+        assertEquals("Hello, World!", main.exec("hello"));
+        assertEquals("Hello, Wisconsin!", main.exec("hello", "--name=Wisconsin"));
+        assertEquals("Hola, Ecuador!", main.exec("hello", "--name=Ecuador", "--language=ES"));
+    }
+}

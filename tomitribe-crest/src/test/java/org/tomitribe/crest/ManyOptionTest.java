6740193d3a6272d0e48923e076fe8770c84f594c
dblevins <david.blevins@gmail.com>
2014-08-27 13:03:52 -0700

    'rsync -del' and 'rsync --del' are two different commands.  We cannot let '-del' imply '--del'
    See:
     rsync -h | egrep '( -[del]|del )'
      -d, --dirs                  transfer directories without recursing
      -l, --links                 copy symlinks as symlinks
      -e, --rsh=COMMAND           specify the remote shell to use
          --del                   an alias for --delete-during

--- tomitribe-crest/src/test/java/org/tomitribe/crest/ManyOptionTest.java
+++ tomitribe-crest/src/test/java/org/tomitribe/crest/ManyOptionTest.java
@@ -17,6 +17,8 @@
 package org.tomitribe.crest;
 
 import junit.framework.TestCase;
+import org.junit.Ignore;
+import org.junit.Test;
 import org.tomitribe.crest.api.Command;
 import org.tomitribe.crest.api.Option;
 import org.tomitribe.crest.cmds.Cmd;
@@ -32,6 +34,119 @@ public class ManyOptionTest extends TestCase{
         commands.get("doString").exec("--foo=2", "--foox=3", "-f=5");
     }
 
+    /**
+     * We must get this test to pass
+     */
+    public void testIllegal() throws Exception {
+        final Cmd rsync = commands.get("rsync");
+
+        // Short options cannot be long options
+        try {
+            rsync.exec("--d");
+
+            fail();
+        } catch (IllegalArgumentException pass) {
+        }
+
+        try {
+            rsync.exec("--e");
+
+            fail();
+        } catch (IllegalArgumentException pass) {
+        }
+
+        try {
+            rsync.exec("--l");
+
+            fail();
+        } catch (IllegalArgumentException pass) {
+        }
+
+        // Long options cannot be short options
+        try {
+            rsync.exec("-del");
+
+            fail();
+        } catch (IllegalArgumentException pass) {
+        }
+
+        try {
+            rsync.exec("-delete-during");
+
+            fail();
+        } catch (IllegalArgumentException pass) {
+        }
+
+        try {
+            rsync.exec("-dirs");
+
+            fail();
+        } catch (IllegalArgumentException pass) {
+        }
+
+        try {
+            rsync.exec("-links");
+
+            fail();
+        } catch (IllegalArgumentException pass) {
+        }
+
+        try {
+            rsync.exec("-rsh");
+
+            fail();
+        } catch (IllegalArgumentException pass) {
+        }
+
+    }
+
+    /**
+     * Optional.  We don't have to support stringing short options together
+     * as is allowed in posix.  Nice to have.  Could easily be done after 1.0.
+     *
+     * We do have to make the above Illegal, however or we'll never be able
+     * support it without potentially breaking someone.  If we make
+     * "-del" illegal we can add posix stringing support any time.
+     */
+    @Ignore
+    public void testMixedLongAndShort() {
+        final Cmd rsync = commands.get("rsync");
+
+        assertCommand(
+                rsync.exec(),
+                "rsync{dirs=null, links=null, verbose=null, delete=null}");
+
+        assertCommand(
+                rsync.exec("-d", "-e", "-l"),
+                "rsync{dirs=true, links=true, verbose=true, delete=null}");
+
+        assertCommand(
+                rsync.exec("-del"),
+                "rsync{dirs=true, links=true, verbose=true, delete=null}");
+
+        assertCommand(
+                rsync.exec("--del"),
+                "rsync{dirs=null, links=null, verbose=null, delete=true}");
+
+        assertCommand(
+                rsync.exec("--no-del"),
+                "rsync{dirs=null, links=null, verbose=null, delete=false}");
+
+        assertCommand(
+                rsync.exec("-d", "-e", "-l", "--no-del"),
+                "rsync{dirs=true, links=true, verbose=true, delete=false}");
+
+
+        assertCommand(
+                rsync.exec("-del", "--no-del"),
+                "rsync{dirs=true, links=true, verbose=true, delete=false}");
+
+    }
+    private void assertCommand(Object result, String expected) {
+        assertEquals(expected, result);
+    }
+
+
     public static class Commands {
 
         @Command
@@ -43,6 +158,22 @@ public class ManyOptionTest extends TestCase{
             assertEquals("3", it.next());
             assertEquals("5", it.next());
             assertFalse(it.hasNext());
+        }
+
+        @Command
+        public String rsync(
+                @Option({"d", "dirs"}) Boolean dirs,
+                @Option({"l", "links"}) Boolean links,
+                @Option({"e", "rsh"}) Boolean verbose,
+                @Option({"del", "delete-during"}) Boolean delete) {
+
+            return "rsync{" +
+                    "dirs=" + dirs +
+                    ", links=" + links +
+                    ", verbose=" + verbose +
+                    ", delete=" + delete +
+                    '}';
+
         }
     }
 }

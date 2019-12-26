dc0e271d7888c990224d81c80c9d87746670f5f3
David Blevins <david.blevins@gmail.com>
2019-12-26 13:32:09 -0800

    Text wrapping pulled from commons-lang3

---
+++ tomitribe-crest/src/main/java/org/tomitribe/crest/help/Wrap.java
@@ -0,0 +1,165 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.tomitribe.crest.help;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Wrap {
+
+    public static int length(final CharSequence cs) {
+        return cs == null ? 0 : cs.length();
+    }
+
+    public static boolean isBlank(final CharSequence cs) {
+        int strLen = length(cs);
+        if (strLen == 0) {
+            return true;
+        }
+        for (int i = 0; i < strLen; i++) {
+            if (!Character.isWhitespace(cs.charAt(i))) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * <p>Wraps a single line of text, identifying words by {@code ' '}.</p>
+     *
+     * <p>New lines will be separated by the system property line separator.
+     * Very long words, such as URLs will <i>not</i> be wrapped.</p>
+     *
+     * <p>Leading spaces on a new line are stripped.
+     * Trailing spaces are not stripped.</p>
+     *
+     * @param str  the String to be word wrapped, may be null
+     * @param wrapLength  the column to wrap the words at, less than 1 is treated as 1
+     * @return a line with newlines inserted, {@code null} if null input
+     */
+    public static String wrap(final String str, final int wrapLength) {
+        return wrap(str, wrapLength, null, false);
+    }
+
+    /**
+     * <p>Wraps a single line of text, identifying words by {@code ' '}.</p>
+     *
+     * <p>Leading spaces on a new line are stripped. Trailing spaces are not stripped.</p>
+     *
+     * @param str  the String to be word wrapped, may be null
+     * @param wrapLength  the column to wrap the words at, less than 1 is treated as 1
+     * @param newLineStr  the string to insert for a new line,
+     *  {@code null} uses the system property line separator
+     * @param wrapLongWords  true if long words (such as URLs) should be wrapped
+     * @return a line with newlines inserted, {@code null} if null input
+     **/
+    public static String wrap(final String str, final int wrapLength, final String newLineStr, final boolean wrapLongWords) {
+        return wrap(str, wrapLength, newLineStr, wrapLongWords, " ");
+    }
+
+    /**
+     * <p>Wraps a single line of text, identifying words by {@code wrapOn}.</p>
+     *
+     * <p>Leading spaces on a new line are stripped.
+     * Trailing spaces are not stripped.</p>
+     *
+     * @param str  the String to be word wrapped, may be null
+     * @param wrapLength  the column to wrap the words at, less than 1 is treated as 1
+     * @param newLineStr  the string to insert for a new line,
+     *  {@code null} uses the system property line separator
+     * @param wrapLongWords  true if long words (such as URLs) should be wrapped
+     * @param wrapOn regex expression to be used as a breakable characters,
+     *               if blank string is provided a space character will be used
+     * @return a line with newlines inserted, {@code null} if null input
+     */
+    public static String wrap(final String str, int wrapLength, String newLineStr, final boolean wrapLongWords, String wrapOn) {
+        if (str == null) {
+            return null;
+        }
+        if (newLineStr == null) {
+            newLineStr = System.lineSeparator();
+        }
+        if (wrapLength < 1) {
+            wrapLength = 1;
+        }
+        if (isBlank(wrapOn)) {
+            wrapOn = " ";
+        }
+        final Pattern patternToWrapOn = Pattern.compile(wrapOn);
+        final int inputLineLength = str.length();
+        int offset = 0;
+        final StringBuilder wrappedLine = new StringBuilder(inputLineLength + 32);
+
+        while (offset < inputLineLength) {
+            int spaceToWrapAt = -1;
+            Matcher matcher = patternToWrapOn.matcher(
+                    str.substring(offset, Math.min((int) Math.min(Integer.MAX_VALUE, offset + wrapLength + 1L), inputLineLength)));
+            if (matcher.find()) {
+                if (matcher.start() == 0) {
+                    offset += matcher.end();
+                    continue;
+                }
+                spaceToWrapAt = matcher.start() + offset;
+            }
+
+            // only last line without leading spaces is left
+            if (inputLineLength - offset <= wrapLength) {
+                break;
+            }
+
+            while (matcher.find()) {
+                spaceToWrapAt = matcher.start() + offset;
+            }
+
+            if (spaceToWrapAt >= offset) {
+                // normal case
+                wrappedLine.append(str, offset, spaceToWrapAt);
+                wrappedLine.append(newLineStr);
+                offset = spaceToWrapAt + 1;
+
+            } else {
+                // really long word or URL
+                if (wrapLongWords) {
+                    // wrap really long word one line at a time
+                    wrappedLine.append(str, offset, wrapLength + offset);
+                    wrappedLine.append(newLineStr);
+                    offset += wrapLength;
+                } else {
+                    // do not wrap really long word, just extend beyond limit
+                    matcher = patternToWrapOn.matcher(str.substring(offset + wrapLength));
+                    if (matcher.find()) {
+                        spaceToWrapAt = matcher.start() + offset + wrapLength;
+                    }
+
+                    if (spaceToWrapAt >= 0) {
+                        wrappedLine.append(str, offset, spaceToWrapAt);
+                        wrappedLine.append(newLineStr);
+                        offset = spaceToWrapAt + 1;
+                    } else {
+                        wrappedLine.append(str, offset, str.length());
+                        offset = inputLineLength;
+                    }
+                }
+            }
+        }
+
+        // Whatever is left in line is short enough to just pass through
+        wrappedLine.append(str, offset, str.length());
+
+        return wrappedLine.toString();
+    }
+}
---
+++ tomitribe-crest/src/test/java/org/tomitribe/crest/help/WrapTest.java
@@ -0,0 +1,185 @@
+package org.tomitribe.crest.help;
+
+import org.junit.Test;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Modifier;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Unit tests for Wrap class.
+ */
+@Deprecated
+public class WrapTest {
+
+    //-----------------------------------------------------------------------
+    @Test
+    public void testConstructor() {
+        assertNotNull(new Wrap());
+        final Constructor<?>[] cons = Wrap.class.getDeclaredConstructors();
+        assertEquals(1, cons.length);
+        assertTrue(Modifier.isPublic(cons[0].getModifiers()));
+        assertTrue(Modifier.isPublic(Wrap.class.getModifiers()));
+        assertFalse(Modifier.isFinal(Wrap.class.getModifiers()));
+    }
+
+    //-----------------------------------------------------------------------
+    @Test
+    public void testWrap_StringInt() {
+        assertNull(Wrap.wrap(null, 20));
+        assertNull(Wrap.wrap(null, -1));
+
+        assertEquals("", Wrap.wrap("", 20));
+        assertEquals("", Wrap.wrap("", -1));
+
+        // normal
+        final String systemNewLine = System.lineSeparator();
+        String input = "Here is one line of text that is going to be wrapped after 20 columns.";
+        String expected = "Here is one line of" + systemNewLine + "text that is going"
+                + systemNewLine + "to be wrapped after" + systemNewLine + "20 columns.";
+        assertEquals(expected, Wrap.wrap(input, 20));
+
+        // long word at end
+        input = "Click here to jump to the commons website - https://commons.apache.org";
+        expected = "Click here to jump" + systemNewLine + "to the commons" + systemNewLine
+                + "website -" + systemNewLine + "https://commons.apache.org";
+        assertEquals(expected, Wrap.wrap(input, 20));
+
+        // long word in middle
+        input = "Click here, https://commons.apache.org, to jump to the commons website";
+        expected = "Click here," + systemNewLine + "https://commons.apache.org," + systemNewLine
+                + "to jump to the" + systemNewLine + "commons website";
+        assertEquals(expected, Wrap.wrap(input, 20));
+
+        // leading spaces on a new line are stripped
+        // trailing spaces are not stripped
+        input = "word1             word2                        word3";
+        expected = "word1  " + systemNewLine + "word2  " + systemNewLine + "word3";
+        assertEquals(expected, Wrap.wrap(input, 7));
+    }
+
+    @Test
+    public void testWrap_StringIntStringBoolean() {
+        assertNull(Wrap.wrap(null, 20, "\n", false));
+        assertNull(Wrap.wrap(null, 20, "\n", true));
+        assertNull(Wrap.wrap(null, 20, null, true));
+        assertNull(Wrap.wrap(null, 20, null, false));
+        assertNull(Wrap.wrap(null, -1, null, true));
+        assertNull(Wrap.wrap(null, -1, null, false));
+
+        assertEquals("", Wrap.wrap("", 20, "\n", false));
+        assertEquals("", Wrap.wrap("", 20, "\n", true));
+        assertEquals("", Wrap.wrap("", 20, null, false));
+        assertEquals("", Wrap.wrap("", 20, null, true));
+        assertEquals("", Wrap.wrap("", -1, null, false));
+        assertEquals("", Wrap.wrap("", -1, null, true));
+
+        // normal
+        String input = "Here is one line of text that is going to be wrapped after 20 columns.";
+        String expected = "Here is one line of\ntext that is going\nto be wrapped after\n20 columns.";
+        assertEquals(expected, Wrap.wrap(input, 20, "\n", false));
+        assertEquals(expected, Wrap.wrap(input, 20, "\n", true));
+
+        // unusual newline char
+        input = "Here is one line of text that is going to be wrapped after 20 columns.";
+        expected = "Here is one line of<br />text that is going<br />to be wrapped after<br />20 columns.";
+        assertEquals(expected, Wrap.wrap(input, 20, "<br />", false));
+        assertEquals(expected, Wrap.wrap(input, 20, "<br />", true));
+
+        // short line length
+        input = "Here is one line";
+        expected = "Here\nis one\nline";
+        assertEquals(expected, Wrap.wrap(input, 6, "\n", false));
+        expected = "Here\nis\none\nline";
+        assertEquals(expected, Wrap.wrap(input, 2, "\n", false));
+        assertEquals(expected, Wrap.wrap(input, -1, "\n", false));
+
+        // system newline char
+        final String systemNewLine = System.lineSeparator();
+        input = "Here is one line of text that is going to be wrapped after 20 columns.";
+        expected = "Here is one line of" + systemNewLine + "text that is going" + systemNewLine
+                + "to be wrapped after" + systemNewLine + "20 columns.";
+        assertEquals(expected, Wrap.wrap(input, 20, null, false));
+        assertEquals(expected, Wrap.wrap(input, 20, null, true));
+
+        // with extra spaces
+        input = " Here:  is  one  line  of  text  that  is  going  to  be  wrapped  after  20  columns.";
+        expected = "Here:  is  one  line\nof  text  that  is \ngoing  to  be \nwrapped  after  20 \ncolumns.";
+        assertEquals(expected, Wrap.wrap(input, 20, "\n", false));
+        assertEquals(expected, Wrap.wrap(input, 20, "\n", true));
+
+        // with tab
+        input = "Here is\tone line of text that is going to be wrapped after 20 columns.";
+        expected = "Here is\tone line of\ntext that is going\nto be wrapped after\n20 columns.";
+        assertEquals(expected, Wrap.wrap(input, 20, "\n", false));
+        assertEquals(expected, Wrap.wrap(input, 20, "\n", true));
+
+        // with tab at wrapColumn
+        input = "Here is one line of\ttext that is going to be wrapped after 20 columns.";
+        expected = "Here is one line\nof\ttext that is\ngoing to be wrapped\nafter 20 columns.";
+        assertEquals(expected, Wrap.wrap(input, 20, "\n", false));
+        assertEquals(expected, Wrap.wrap(input, 20, "\n", true));
+
+        // difference because of long word
+        input = "Click here to jump to the commons website - https://commons.apache.org";
+        expected = "Click here to jump\nto the commons\nwebsite -\nhttps://commons.apache.org";
+        assertEquals(expected, Wrap.wrap(input, 20, "\n", false));
+        expected = "Click here to jump\nto the commons\nwebsite -\nhttps://commons.apac\nhe.org";
+        assertEquals(expected, Wrap.wrap(input, 20, "\n", true));
+
+        // difference because of long word in middle
+        input = "Click here, https://commons.apache.org, to jump to the commons website";
+        expected = "Click here,\nhttps://commons.apache.org,\nto jump to the\ncommons website";
+        assertEquals(expected, Wrap.wrap(input, 20, "\n", false));
+        expected = "Click here,\nhttps://commons.apac\nhe.org, to jump to\nthe commons website";
+        assertEquals(expected, Wrap.wrap(input, 20, "\n", true));
+    }
+
+    @Test
+    public void testWrap_StringIntStringBooleanString() {
+
+        //no changes test
+        String input = "flammable/inflammable";
+        String expected = "flammable/inflammable";
+        assertEquals(expected, Wrap.wrap(input, 30, "\n", false, "/"));
+
+        // wrap on / and small width
+        expected = "flammable\ninflammable";
+        assertEquals(expected, Wrap.wrap(input, 2, "\n", false, "/"));
+
+        // wrap long words on / 1
+        expected = "flammable\ninflammab\nle";
+        assertEquals(expected, Wrap.wrap(input, 9, "\n", true, "/"));
+
+        // wrap long words on / 2
+        expected = "flammable\ninflammable";
+        assertEquals(expected, Wrap.wrap(input, 15, "\n", true, "/"));
+
+        // wrap long words on / 3
+        input = "flammableinflammable";
+        expected = "flammableinflam\nmable";
+        assertEquals(expected, Wrap.wrap(input, 15, "\n", true, "/"));
+    }
+
+
+    @Test
+    public void testLANG1292() {
+        // Prior to fix, this was throwing StringIndexOutOfBoundsException
+        Wrap.wrap("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa "
+                + "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa "
+                + "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", 70);
+    }
+
+    @Test
+    public void testLANG1397() {
+        // Prior to fix, this was throwing StringIndexOutOfBoundsException
+        Wrap.wrap("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa "
+                + "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa "
+                + "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", Integer.MAX_VALUE);
+    }
+}

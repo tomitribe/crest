2e6240c389ca3b6f2a3a1f5ca07f5856a97f211c
Romain Manni-Bucau <rmannibucau@gmail.com>
2021-11-01 19:16:41 +0100

    tomitribe-crest-arthur-extension contribution

---
+++ tomitribe-crest-arthur-extension/src/test/java/org/tomitribe/crest/arthur/CrestExtensionTest.java
@@ -0,0 +1,78 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.tomitribe.crest.arthur;
+
+import org.apache.geronimo.arthur.impl.nativeimage.ArthurNativeImageConfiguration;
+import org.apache.geronimo.arthur.impl.nativeimage.generator.DefautContext;
+import org.apache.geronimo.arthur.spi.model.ClassReflectionModel;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+import org.tomitribe.crest.api.Command;
+
+import java.lang.reflect.Method;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Collection;
+
+import static java.util.Collections.singleton;
+import static java.util.Collections.singletonList;
+import static java.util.Collections.singletonMap;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class CrestExtensionTest {
+    @Rule
+    public final TemporaryFolder temp = new TemporaryFolder();
+
+    @Test
+    public void generate() throws Exception {
+        final Method foo = Enclosing.class.getMethod("foo");
+
+        final CrestExtension extension = new CrestExtension();
+        final ArthurNativeImageConfiguration configuration = new ArthurNativeImageConfiguration();
+        final DefautContext context = new DefautContext(
+                configuration,
+                annot -> {
+                    throw new UnsupportedOperationException();
+                },
+                annot -> singleton(foo),
+                annot -> {
+                    throw new UnsupportedOperationException();
+                },
+                a -> {
+                    throw new UnsupportedOperationException();
+                },
+                singletonMap("workingDirectory", temp.getRoot().getAbsolutePath()));
+        extension.execute(context);
+
+        final Collection<ClassReflectionModel> reflections = context.getReflections();
+        assertEquals(1, reflections.size());
+
+        final Path spiFile = temp.getRoot().toPath().resolve("crest-commands.txt");
+        assertTrue(Files.exists(spiFile));
+        assertEquals(singletonList(Enclosing.class.getName()), Files.readAllLines(spiFile));
+
+        assertEquals(singletonList("-H:TomitribeCrestCommands=" + spiFile), configuration.getCustomOptions());
+    }
+
+    public static class Enclosing {
+        @Command
+        public static void foo() {
+        }
+    }
+}

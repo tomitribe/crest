acd6a961cfd18b2cb658fc3d9ab98eeee74de391
David Blevins <dblevins@tomitribe.com>
2022-03-23 00:12:59 -0700

    Javadoc each Border with an example Put borders in alphabetical order

--- tomitribe-crest-api/src/main/java/org/tomitribe/crest/api/table/Border.java
+++ tomitribe-crest-api/src/main/java/org/tomitribe/crest/api/table/Border.java
@@ -16,17 +16,182 @@
 package org.tomitribe.crest.api.table;
 
 public enum Border {
-    whitespaceSeparated,
-    whitespaceCompact,
-    mysqlStyle,
-    asciiSeparated,
+
+    /**
+     * Example:
+     * <pre>
+     *     id               project           releaseDate   version
+     * ----------- ------------------------- ------------- ---------
+     *          9   Apache TomEE              2016-05-17    7.0.x
+     *  523456789   Tomcat                    2018-01-17    9.0.x
+     *         14   Apache ActiveMQ Classic   2022-03-09    5.17.x
+     * </pre>
+     */
     asciiCompact,
-    githubMarkdown,
-    redditMarkdown,
-    reStructuredTextGrid,
-    reStructuredTextSimple,
+
+    /**
+     * Example:
+     * <pre>
+     * ...............................................................
+     * :    id     :         project         : releaseDate : version :
+     * :...........:.........................:.............:.........:
+     * :         9 : Apache TomEE            : 2016-05-17  : 7.0.x   :
+     * : 523456789 : Tomcat                  : 2018-01-17  : 9.0.x   :
+     * :        14 : Apache ActiveMQ Classic : 2022-03-09  : 5.17.x  :
+     * :...........:.........................:.............:.........:
+     * </pre>
+     */
     asciiDots,
+
+    /**
+     * Example:
+     * <pre>
+     * +===========+=========================+=============+=========+
+     * |    id     |         project         | releaseDate | version |
+     * +===========+=========================+=============+=========+
+     * |         9 | Apache TomEE            | 2016-05-17  | 7.0.x   |
+     * +-----------+-------------------------+-------------+---------+
+     * | 523456789 | Tomcat                  | 2018-01-17  | 9.0.x   |
+     * +-----------+-------------------------+-------------+---------+
+     * |        14 | Apache ActiveMQ Classic | 2022-03-09  | 5.17.x  |
+     * +-----------+-------------------------+-------------+---------+
+     * </pre>
+     */
+    asciiSeparated,
+
+    /**
+     * Example:
+     * <pre>
+     * |    id     |         project         | releaseDate | version |
+     * |-----------|-------------------------|-------------|---------|
+     * |         9 | Apache TomEE            | 2016-05-17  | 7.0.x   |
+     * | 523456789 | Tomcat                  | 2018-01-17  | 9.0.x   |
+     * |        14 | Apache ActiveMQ Classic | 2022-03-09  | 5.17.x  |
+     * </pre>
+     */
+    githubMarkdown,
+
+    /**
+     * Example:
+     * <pre>
+     * +-----------+-------------------------+-------------+---------+
+     * |    id     |         project         | releaseDate | version |
+     * +-----------+-------------------------+-------------+---------+
+     * |         9 | Apache TomEE            | 2016-05-17  | 7.0.x   |
+     * | 523456789 | Tomcat                  | 2018-01-17  | 9.0.x   |
+     * |        14 | Apache ActiveMQ Classic | 2022-03-09  | 5.17.x  |
+     * +-----------+-------------------------+-------------+---------+
+     * </pre>
+     */
+    mysqlStyle,
+
+    /**
+     * Example:
+     * <pre>
+     * +-----------+-------------------------+-------------+---------+
+     * |    id     |         project         | releaseDate | version |
+     * +===========+=========================+=============+=========+
+     * |         9 | Apache TomEE            | 2016-05-17  | 7.0.x   |
+     * | 523456789 | Tomcat                  | 2018-01-17  | 9.0.x   |
+     * |        14 | Apache ActiveMQ Classic | 2022-03-09  | 5.17.x  |
+     * +-----------+-------------------------+-------------+---------+
+     * </pre>
+     */
+    reStructuredTextGrid,
+
+    /**
+     * Example:
+     * <pre>
+     * =========== ========================= ============= =========
+     *     id               project           releaseDate   version
+     * =========== ========================= ============= =========
+     *          9   Apache TomEE              2016-05-17    7.0.x
+     *  523456789   Tomcat                    2018-01-17    9.0.x
+     *         14   Apache ActiveMQ Classic   2022-03-09    5.17.x
+     * =========== ========================= ============= =========
+     * </pre>
+     */
+    reStructuredTextSimple,
+
+    /**
+     * Example:
+     * <pre>
+     *     id     |         project         | releaseDate | version
+     * -----------|-------------------------|-------------|---------
+     *          9 | Apache TomEE            | 2016-05-17  | 7.0.x
+     *  523456789 | Tomcat                  | 2018-01-17  | 9.0.x
+     *         14 | Apache ActiveMQ Classic | 2022-03-09  | 5.17.x
+     * </pre>
+     */
+    redditMarkdown,
+
+    /**
+     * Example:
+     * <pre>
+     * ╔═══════════╦═════════════════════════╦═════════════╦═════════╗
+     * ║    id     ║         project         ║ releaseDate ║ version ║
+     * ╠═══════════╬═════════════════════════╬═════════════╬═════════╣
+     * ║         9 ║ Apache TomEE            ║ 2016-05-17  ║ 7.0.x   ║
+     * ║ 523456789 ║ Tomcat                  ║ 2018-01-17  ║ 9.0.x   ║
+     * ║        14 ║ Apache ActiveMQ Classic ║ 2022-03-09  ║ 5.17.x  ║
+     * ╚═══════════╩═════════════════════════╩═════════════╩═════════╝
+     * </pre>
+     */
     unicodeDouble,
+
+    /**
+     * Example:
+     * <pre>
+     * ┌───────────┬─────────────────────────┬─────────────┬─────────┐
+     * │    id     │         project         │ releaseDate │ version │
+     * ├───────────┼─────────────────────────┼─────────────┼─────────┤
+     * │         9 │ Apache TomEE            │ 2016-05-17  │ 7.0.x   │
+     * │ 523456789 │ Tomcat                  │ 2018-01-17  │ 9.0.x   │
+     * │        14 │ Apache ActiveMQ Classic │ 2022-03-09  │ 5.17.x  │
+     * └───────────┴─────────────────────────┴─────────────┴─────────┘
+     * </pre>
+     */
     unicodeSingle,
-    unicodeSingleSeparated
+
+    /**
+     * Example:
+     * <pre>
+     * ┌───────────┬─────────────────────────┬─────────────┬─────────┐
+     * │    id     │         project         │ releaseDate │ version │
+     * ├═══════════┼═════════════════════════┼═════════════┼═════════┤
+     * │         9 │ Apache TomEE            │ 2016-05-17  │ 7.0.x   │
+     * ├───────────┼─────────────────────────┼─────────────┼─────────┤
+     * │ 523456789 │ Tomcat                  │ 2018-01-17  │ 9.0.x   │
+     * ├───────────┼─────────────────────────┼─────────────┼─────────┤
+     * │        14 │ Apache ActiveMQ Classic │ 2022-03-09  │ 5.17.x  │
+     * └───────────┴─────────────────────────┴─────────────┴─────────┘
+     * </pre>
+     */
+    unicodeSingleSeparated,
+
+    /**
+     * Example:
+     * <pre>
+     *    id               project           releaseDate   version
+     *
+     *         9   Apache TomEE              2016-05-17    7.0.x
+     * 523456789   Tomcat                    2018-01-17    9.0.x
+     *        14   Apache ActiveMQ Classic   2022-03-09    5.17.x
+     * </pre>
+     */
+    whitespaceCompact,
+
+    /**
+     * Example:
+     * <pre>
+     *    id               project           releaseDate   version
+     *
+     *         9   Apache TomEE              2016-05-17    7.0.x
+     *
+     * 523456789   Tomcat                    2018-01-17    9.0.x
+     *
+     *        14   Apache ActiveMQ Classic   2022-03-09    5.17.x
+     * </pre>
+     */
+    whitespaceSeparated,
 }
